949a4d74a1c0e4d880ac44d2632b5db94d5ab560
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-09-08 20:07:35 +0300

    Introduced vehicle map.

--- src/main/java/Context.java
+++ src/main/java/Context.java
@@ -6,6 +6,9 @@ public final class Context {
 
   private static final Logger LOGGER = Logger.get(Context.class);
 
+  private final VehicleMap ally = new VehicleMap();
+  private final VehicleMap enemy = new VehicleMap();
+
   private State state;
 
   public Context() {
@@ -22,5 +25,19 @@ public final class Context {
   }
 
   public void update(World world) {
+    long playerId = world.getMyPlayer().getId();
+    for (Vehicle vehicle : world.getNewVehicles()) {
+      if (vehicle.getPlayerId() == playerId) {
+        ally.add(vehicle);
+      } else {
+        enemy.add(vehicle);
+      }
+    }
+
+    for (VehicleUpdate u : world.getVehicleUpdates()) {
+      if (!ally.update(u)) {
+        enemy.update(u);
+      }
+    }
   }
 }
---
+++ src/main/java/VehicleMap.java
@@ -0,0 +1,11 @@
+import model.Vehicle;
+import model.VehicleUpdate;
+
+public class VehicleMap {
+  public void add(Vehicle vehicle) {
+  }
+
+  public boolean update(VehicleUpdate u) {
+    return false;
+  }
+}
