536ae668945d5cef0da9ccccb4acfe4957174812
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-09-09 20:20:04 +0300

    Added support for vehicle update.

--- src/main/java/VehicleMap.java
+++ src/main/java/VehicleMap.java
@@ -1,11 +1,30 @@
 import model.Vehicle;
 import model.VehicleUpdate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VehicleMap {
+
+  private final Map<Long, Vehicle> vehicles = new HashMap<>(500);
+
   public void add(Vehicle vehicle) {
+    vehicles.put(vehicle.getId(), vehicle);
   }
 
   public boolean update(VehicleUpdate u) {
-    return false;
+    long id = u.getId();
+    Vehicle vehicle = vehicles.get(id);
+    if (vehicle == null) {
+      return false;
+    }
+
+    if (u.getDurability() > 0) {
+      vehicles.put(id, new Vehicle(vehicle, u));
+    } else {
+      vehicles.remove(id);
+    }
+
+    return true;
   }
 }
