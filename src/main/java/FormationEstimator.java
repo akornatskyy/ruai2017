bcb481aaa70ae7f2e4023bc881e347166f44d735
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-09-28 20:27:44 +0300

    Updated formation estimator to choose target per preferred setup.

--- src/main/java/FormationEstimator.java
+++ src/main/java/FormationEstimator.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
 import java.util.List;
 
 public final class FormationEstimator implements Estimator {
+
+  private static final Vector[][] TARGETS = Arrays.stream(
+      new double[][][] {
+          {{0, 0}, {1, 0}, {0, 1}},
+          {{1, 2}, {2, 1}, {0, 2}, {2, 0}, {2, 2}},
+          {{1, 1}, {0, 1}, {1, 0}},
+          {{1, 1}, {0, 1}, {1, 0}},
+          {{0, 2}, {2, 0}, {1, 2}, {2, 1}, {1, 1}}
+      })
+      .map(route -> Arrays.stream(route)
+          .map(c -> {
+            Vector v = new Vector();
+            v.set(c[0] * 75 + 45, c[1] * 75 + 45);
+            return v;
+          })
+          .toArray(Vector[]::new))
+      .toArray(Vector[][]::new);
 
   private final List<VehicleGroup> allies;
 
@@ -10,22 +28,38 @@ public final class FormationEstimator implements Estimator {
 
   @Override
   public double calc(VehicleGroup group, Vector target) {
-    double s = 0.0;
+    double p = attractive(group, target);
     for (VehicleGroup other : allies) {
       if (group != other) {
-        s += attractive(group, target) + repulsive(group, other, target);
+        p += repulsive(group, other, target);
       }
     }
 
-    return s;
+    // does not move at exp^(-745) == 0
+    final double WALL = 0.06;
+    p -= Math.exp((-target.x + 18) / WALL) // left
+         + Math.exp((-target.y + 18) / WALL); // top
+
+    return p;
   }
 
   private static double attractive(VehicleGroup group, Vector target) {
-    return 0;
+    return Arrays.stream(TARGETS[group.getGroupId()])
+        .mapToDouble(t -> Math.exp(-target.len(t)))
+        .max()
+        .orElse(0);
   }
 
   private static double repulsive(
       VehicleGroup group, VehicleGroup other, Vector target) {
-    return 0;
+    double p = 0;
+    if (group.canCollide(other)) {
+      double d = target.len(other.getCenter());
+      if (d <= 96) {
+        p = -Math.pow(d, -2);
+      }
+    }
+
+    return p;
   }
 }
--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -6,4 +6,15 @@ public final class Vector {
     this.x = x;
     this.y = y;
   }
+  
+  public double len(Vector other) {
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("(%.2f, %.2f)", x, y);
+  }
 }
